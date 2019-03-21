212697d8b56f4297c5a9bb25560b866ad9526588
redtaq <redtaq@hotmail.com>
2019-03-21 16:03:19 -0500

    Updates

---
+++ precache-manifest.502ee9c9d3a7d2368ab7c5428ea5fa85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e87183ccbc5b63a08e7f",
+    "url": "/trivia_game_react/static/css/main.26b6ce79.chunk.css"
+  },
+  {
+    "revision": "e87183ccbc5b63a08e7f",
+    "url": "/trivia_game_react/static/js/main.c387349d.chunk.js"
+  },
+  {
+    "revision": "369d35e9e8c22c0e9503",
+    "url": "/trivia_game_react/static/js/runtime~main.7eddbb2d.js"
+  },
+  {
+    "revision": "30c5ed4d15ca3062879a",
+    "url": "/trivia_game_react/static/css/2.df2272f2.chunk.css"
+  },
+  {
+    "revision": "30c5ed4d15ca3062879a",
+    "url": "/trivia_game_react/static/js/2.ea97f727.chunk.js"
+  },
+  {
+    "revision": "822ae93c45ebef69d9a0e8ffb3ba32ae",
+    "url": "/trivia_game_react/index.html"
+  }
+];
